424353a83e37833ec998adf6c4cacfdcef36e853
freddan88 <fredrik@leemann.se>
2020-05-16 13:07:41 +0200

    Prepare: Test if send email works from webpage

--- index.php
+++ index.php
@@ -3,23 +3,54 @@
 declare(strict_types=1);
 require('./app.conf.php');
 
+function addError($field)
+{
+    array_push($_SESSION['errors'], $field . " is missing");
+}
+
 function endSession()
 {
+    session_start();
     session_destroy();
     exit;
 }
 
 function generateToken()
 {
+    session_start();
     $_SESSION['csrf'] = bin2hex(random_bytes(32));
-    $json = json_encode($_SESSION);
-    echo $json;
+    echo json_encode($_SESSION['csrf']);
 }
 
 function validateSendMail()
 {
-    $json = json_encode($_SESSION);
-    echo $json;
+    session_start();
+    if (!isset($_POST['csrf']) || empty($_POST['csrf'])) endSession();
+    if ($_POST['csrf'] !== $_SESSION['csrf']) endSession();
+    $_SESSION['errors'] = [];
+
+    if (!isset($_POST['message']) || empty($_POST['message'])) addError("message");
+    if (!isset($_POST['fullname']) || empty($_POST['fullname'])) addError("fullname");
+    if (!isset($_POST['email_from']) || empty($_POST['email_from'])) addError("email_from");
+
+    if (count($_SESSION['errors']) > 0) {
+        echo json_encode($_SESSION['errors']);
+        endSession();
+    }
+
+    $message = trim(filter_var($_POST['message'], FILTER_SANITIZE_STRING));
+    $fullname = trim(filter_var($_POST['fullname'], FILTER_SANITIZE_STRING));
+    $email_from = trim(filter_var($_POST['email_from'], FILTER_SANITIZE_EMAIL));
+    $body = $message . "\n" . "From: " . $fullname . "<" . $email_from . ">";
+    $body = wordwrap($body, 80);
+
+    try {
+        mail("fredrik@leemann.se", "New message from website", $body);
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
+
+    echo json_encode($_SESSION);
     endSession();
 }
 
@@ -29,7 +60,6 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     if ($_POST['origin'] !== $ALLOWED_ORIGIN) exit;
     if ($_POST['apikey'] !== $VALID_API_KEY) exit;
     $req = $_SERVER['REQUEST_URI'];
-    session_start();
 
     // Route: Generate new csrf-token
     if ($req === "/token") generateToken();
@@ -40,17 +70,3 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     // Route: Destroy active session
     if ($req === "/session") endSession();
 }
-
-if (isset($_POST['origin'], $_POST['csrf'])) {
-    $origin = $_POST['origin'];
-    $token = $_POST['csrf'];
-    if ($origin === 'localhost:8000' && $token === $_SESSION['csrf']) {
-        $json = json_encode($_SESSION);
-        echo $json;
-    }
-}
-
-$email_to = trim(filter_var($_POST['email_to'], FILTER_SANITIZE_EMAIL));
-$email_from = trim(filter_var($_POST['email_from'], FILTER_SANITIZE_EMAIL));
-$fullname = trim(filter_var($_POST['fullname'], FILTER_SANITIZE_STRING));
-$message = trim(filter_var($_POST['message'], FILTER_SANITIZE_STRING));
